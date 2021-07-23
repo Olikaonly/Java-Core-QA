0a5e7d4899374a0bf593455763833617829bfb38
Olga Shestakova <eolgis@gmail.com>
2021-07-24 00:32:34 +0700

    Create Fruit.java

---
+++ src/main/java/DZ03/Fruit.java
@@ -0,0 +1,17 @@
+package DZ03;
+
+/**
+ * Created by Olga Shestakova
+ * Date 23.07.2021
+ */
+public class Fruit {
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
