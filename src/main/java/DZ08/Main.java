147845c704baab5acc8f2545d17a421a84a1dab9
Olga Shestakova <eolgis@gmail.com>
2021-08-12 21:48:59 +0700

    Create Main.java

---
+++ src/main/java/DZ08/Main.java
@@ -0,0 +1,13 @@
+package DZ08;
+
+/**
+ * Created by Olga Shestakova
+ * Date 10.08.2021
+ */
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
