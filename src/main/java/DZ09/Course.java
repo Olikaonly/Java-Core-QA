aa8dba23cc30cfecd4cd6c6480ff3ff68fe6ec55
Olga Shestakova <eolgis@gmail.com>
2021-08-16 15:07:06 +0700

    Create Course.java

---
+++ src/main/java/DZ09/Course.java
@@ -0,0 +1,43 @@
+package DZ09;
+
+import java.util.Objects;
+
+/**
+ * Created by Olga Shestakova
+ * Date 16.08.2021
+ */
+public class Course {
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(name, course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
